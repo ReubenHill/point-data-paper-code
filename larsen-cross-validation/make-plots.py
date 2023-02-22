002169b873d1abd32314344e8fd6536d135cf197
Daniel Shapero <shapero.daniel@gmail.com>
2023-02-22 11:44:20 -0800

    Show training data point locations

--- larsen-cross-validation/make-plots.py
+++ larsen-cross-validation/make-plots.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 import rasterio
+import xarray
 import firedrake
 import icepack
 
@@ -17,7 +18,7 @@ ax.set_title("Cross-validation error")
 fig.savefig("xval-test-errors.png", dpi=150)
 
 
-# Plot some values of the inferred log-fluidity
+# Get the simulation data
 αs = np.linspace(2750.0, 5000.0, 10)
 θs = []
 
@@ -26,11 +27,21 @@ with firedrake.CheckpointFile("larsen.h5", "r") as chk:
     for α in αs:
         θs.append(chk.load_function(mesh, f"log_fluidity-{α}"))
 
+    training_indices = np.array(chk.h5pyfile["training_indices"])
+
 coords = mesh.coordinates.dat.data_ro
-δ = 50e3
+δ = 10e3
 xmin, xmax = coords[:, 0].min() - δ, coords[:, 0].max() + δ
 ymin, ymax = coords[:, 1].min() - δ, coords[:, 1].max() + δ
 
+# Get the training points
+velocity_filename = icepack.datasets.fetch_measures_antarctica()
+kw = {"x": slice(xmin, xmax), "y": slice(ymax, ymin)}
+dataset = xarray.open_dataset(velocity_filename).sel(**kw)
+X, Y = dataset["x"], dataset["y"]
+xs = np.array([(X[j], Y[i]) for i, j in training_indices])
+
+# Fetch a satellite image
 image_filename = icepack.datasets.fetch_mosaic_of_antarctica()
 with rasterio.open(image_filename, "r") as image_file:
     transform = image_file.transform
@@ -43,10 +54,11 @@ with rasterio.open(image_filename, "r") as image_file:
     )
     image = image_file.read(indexes=1, window=window, masked=True)
 
+# Plot some of the log-fluidity values
 fig, axes = plt.subplots(
     nrows=1, ncols=2, sharex=True, sharey=True, figsize=(6.4, 3.6)
 )
-xmin, ymin, xmax, ymax = rasterio.windows.bounds(window, transform)
+#xmin, ymin, xmax, ymax = rasterio.windows.bounds(window, transform)
 kw = {
     "extent": (xmin, xmax, ymin, ymax),
     "cmap": "Greys_r",
@@ -64,3 +76,14 @@ axes[0].set_title(f"$\\alpha$ = {αs[2] / 1e3}km")
 axes[1].set_title(f"$\\alpha$ = {αs[-1] / 1e3}km")
 fig.suptitle("Log-fluidities at different regularization")
 fig.savefig("xval-log-fluidities.png", dpi=150, bbox_inches="tight")
+
+# Show where the training points are
+fig, axes = plt.subplots()
+axes.set_aspect("equal")
+axes.imshow(image, **kw)
+axes.set_xlim((-2.075e6, -2.025e6))
+axes.set_ylim((1.125e6, 1.175e6))
+firedrake.triplot(mesh, axes=axes)
+axes.scatter(xs[:, 0], xs[:, 1], marker=".")
+axes.set_title("Training point locations")
+fig.savefig("xval-training-points.png", dpi=150, bbox_inches="tight")
